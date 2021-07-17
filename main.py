aad74af2dffb410124fa1fc00426fb9a21fc5a91
Akhilesh Balaji <69246693+akhilesh-balaji@users.noreply.github.com>
2021-07-17 08:11:18 +0530

    Fixed bug where new window's parent would hang.

--- main.py
+++ main.py
@@ -129,16 +129,13 @@ def createNewWindow(_=False):
     """Creating a new window"""
 
     # Check whether the exe or the python script is being used
-    try:
-        exePath = f"{os.path.dirname(os.path.realpath(__file__))}" \
-            "\\Textylic.exe"
-        subprocess.check_call(
-            exePath,
-            shell=True)
+    exePath = f"{os.path.dirname(os.path.realpath(__file__))}" \
+        "\\Textylic.exe"
+    if os.path.isfile(exePath):
         subprocess.Popen(
             exePath,
             shell=True)
-    except subprocess.CalledProcessError:
+    else:
         subprocess.Popen(
             f"python {os.path.dirname(os.path.realpath(__file__))}\\main.py",
             shell=True)
